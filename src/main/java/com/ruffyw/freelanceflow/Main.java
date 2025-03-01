e7ffd2a6e47d78be5bd330af18af23e92b9ad767
Your Name <you@example.com>
2025-03-01 13:10:10 +0100

    Einfache GUI mit Label erweitert

--- src/main/java/com/ruffyw/freelanceflow/Main.java
+++ src/main/java/com/ruffyw/freelanceflow/Main.java
@@ -3,6 +3,7 @@ package com.ruffyw.freelanceflow;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -11,11 +12,12 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         Button startTimerButton = new Button("Timer starten");
         Button createInvoiceButton = new Button("Rechnung erstellen");
+        Label timeLabel = new Label("Zeit heute: 00:00:00"); // Neues Label
 
         startTimerButton.setOnAction(e -> System.out.println("Timer gestartet"));
         createInvoiceButton.setOnAction(e -> System.out.println("Rechnung wird erstellt"));
 
-        VBox root = new VBox(10, startTimerButton, createInvoiceButton);
+        VBox root = new VBox(10, startTimerButton, createInvoiceButton, timeLabel);
         Scene scene = new Scene(root, 300, 200);
         primaryStage.setTitle("FreelanceFlow");
         primaryStage.setScene(scene);
