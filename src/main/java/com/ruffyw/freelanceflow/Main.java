d6be55ac67e43c422813b6565312e04257705e20
Your Name <you@example.com>
2025-03-01 14:33:43 +0100

    Tabs angeordnet.

--- src/main/java/com/ruffyw/freelanceflow/Main.java
+++ src/main/java/com/ruffyw/freelanceflow/Main.java
@@ -2,10 +2,7 @@ package com.ruffyw.freelanceflow;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.animation.Timeline;
@@ -24,27 +21,53 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         startTimerButton = new Button("Timer starten");
-        Button createInvoiceButton = new Button("Rechnung erstellen");
         timeLabel = new Label("Zeit heute: 00:00:00");
 
         startTimerButton.setOnAction(e -> toggleTimer());
-        createInvoiceButton.setOnAction(e -> System.out.println("Rechnung wird erstellt"));
+
 
         timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> updateTime()));
         timeline.setCycleCount(Timeline.INDEFINITE);
 
-        VBox timeBox = new VBox(10, startTimerButton, createInvoiceButton, timeLabel);
+        VBox timeBox = new VBox(10, startTimerButton, timeLabel);
+        timeBox.setSpacing(15);
 
         //Tabs
         TabPane tabPane = new TabPane();
         Tab timeTab = new Tab("Zeit", timeBox);
-        Tab customersTab = new Tab("Kunden", new Label("KundenBereich - soon"));
-        Tab tasksTab = new Tab("Aufgaben", new Label("AufgabenBereich - soon"));
-        Tab statsTab = new Tab("Statistik", new Label("StatistikBereich - soon"));
+        timeTab.setClosable(false);
+
+
+        //KundenTab
+        VBox customerBox = new VBox(10);
+        Button bill = new Button("Rechnung erstellen");
+        Label billStatus = new Label("Keine Rechnung");
+        bill.setOnAction(e -> billStatus.setText("Rechnung wird erstellt"));
+        customerBox.getChildren().addAll(
+                new Label("Kundenliste noch in der Entwicklung"),
+                new Label("Noch in Entwicklung"),
+                bill,
+                billStatus
+        );
+        Tab customersTab = new Tab("Kunden", customerBox);
+        customersTab.setClosable(false);
+
+        //TasksTab
+        VBox tasksBox = new VBox(10);
+        tasksBox.getChildren().add(new TextField("Aufgabe eingeben"));
+        Tab tasksTab = new Tab("Aufgaben", tasksBox);
+        tasksTab.setClosable(false);
+
+        //StatsTab
+        VBox statsBox = new VBox(10);
+        statsBox.getChildren().add(new Label("StatistikBereich - bald verfügbar"));
+        Tab statsTab = new Tab("Statistik", statsBox);
+        statsTab.setClosable(false);
+
 
         tabPane.getTabs().addAll(timeTab, customersTab, tasksTab, statsTab);
 
-        Scene scene = new Scene(tabPane, 300, 200);
+        Scene scene = new Scene(tabPane, 500, 400);
         primaryStage.setTitle("FreelanceFlow");
         primaryStage.setScene(scene);
         primaryStage.show();
